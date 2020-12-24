240ada1512365624859aec30331c4a6511354110
yudis <yudis@live.cn>
2020-12-24 10:12:50 +0800

    Update widget.dart

--- widget.dart
+++ widget.dart
@@ -82,3 +82,60 @@ flexibleSpace: Container(
       colors: [Color(0XFFb2dfdb), Color(0XFF4d9887), Color(0XFF006961)]),
   ),
 )
+  
+/** 调整Switch组件尺寸 **/
+Transform.scale(
+  scale: 3.0,
+  child: Switch(
+      value: _enabled,
+      onChanged: (bool) {
+        _enabled = bool;
+        setState(() {});
+      }),
+)
+  
+/** 404页面 **/
+  
+ onUnknownRoute: (settings) {
+  return MaterialPageRoute(builder: (_) => PageNotFound());
+},
+
+/** 指定TextField文本框初始值 **/
+ 
+  TextField(
+    controller: TextEditingController()..text="你今天真好看",
+  ),
+
+  TextFormField(
+    initialValue: "我今天真好看",
+  )
+
+ /** 根据屏幕尺寸的百分比设定元素尺寸 **/
+  
+  /// 方法一、获取屏幕尺寸
+  // 在第一个`Container`内添加
+  height: MediaQuery.of(context).size.height,
+  width: MediaQuery.of(context).size.width * 0.3,
+ // 在第二个`Container`内添加
+  height: MediaQuery.of(context).size.height,
+  width: MediaQuery.of(context).size.width * 0.7,
+   
+ /// 方法二、使用Expanded.Flex
+
+  body: Row(
+    children: [
+      Expanded(
+        flex: 3,
+        child: Container(color: Color(0xFFFF6666), child: Center(child: Text("30%", style: Theme.of(context).textTheme.headline2,),),),
+      ),
+      Expanded(
+        flex: 7,
+        child: Container(color: Color(0xFFFFFF66), child: Center(child: Text("70%", style: Theme.of(context).textTheme.headline2,),)),
+      )
+    ],
+  ),
+
+/** 隐藏/显示某`widget`组件、切换显示状态 **/
+
+
+ 
